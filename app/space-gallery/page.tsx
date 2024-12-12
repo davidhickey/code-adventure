2d1a83ed2866767a76dddb3ab7ddff8cd5e865a3
davidhickey <david@ecosapiens.xyz>
2024-12-12 12:45:56 -0500

    fixes deployment issue

--- app/space-gallery/page.tsx
+++ app/space-gallery/page.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from "next";
-// import { notFound } from "next/navigation";
-// import ImageGallery from "@/components/sections/ImageGallery";
-// import { fetchApodData } from "@/lib/apis/nasa";
-// import { transformNasaImagesToPhotoAlbum } from "@/components/sections/ImageGallery/helpers";
+import { notFound } from "next/navigation";
+import ImageGallery from "@/components/sections/ImageGallery";
+import { fetchApodData } from "@/lib/apis/nasa";
+import { transformNasaImagesToPhotoAlbum } from "@/components/sections/ImageGallery/helpers";
 
 const SpaceGallery: NextPage = async () => {
-  // const nasaImages = await fetchApodData(20);
-  // if (!nasaImages) {
-  //   return notFound();
-  // }
+  const nasaImages = await fetchApodData(20);
+  if (!nasaImages) {
+    return notFound();
+  }
 
   return (
     <>
-      {/* {(nasaImages && nasaImages?.length > 0) && <ImageGallery title={"NASA Pictures of the Day"} album={transformNasaImagesToPhotoAlbum(nasaImages)} />} */}
+      {(nasaImages && nasaImages?.length > 0) && <ImageGallery title={"NASA Pictures of the Day"} album={transformNasaImagesToPhotoAlbum(nasaImages)} />}
     </>
   );
 };
--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -1,16 +1,16 @@
-// import HeroSection from "@/components/sections/Hero";
+import HeroSection from "@/components/sections/Hero";
 import FeatureSection from "../sections/Feature";
 import CalendarWidget from "../CalendarWidget";
-// import { fetchApodData } from "@/lib/apis/nasa";
+import { fetchApodData } from "@/lib/apis/nasa";
 
 
 const HomePage = async ({children}:{children?: React.ReactNode}) => {
 
-  // const apodData = await fetchApodData();
+  const apodData = await fetchApodData();
 
   return (
     <div>
-      {/* {apodData?.url  && <HeroSection apodData={apodData} />} */}
+      {apodData?.url  && <HeroSection apodData={apodData} />}
       <FeatureSection />
       <CalendarWidget />
       {children && <div>{children}</div>}
