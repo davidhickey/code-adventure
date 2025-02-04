14bbe5debd2496ef93aee95f10ff74cd1c835256
davidhickey <david@ecosapiens.xyz>
2025-02-04 13:47:45 -0500

    fixes type error

--- app/lyrics-game/page.tsx
+++ app/lyrics-game/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import WordGallery from "@/components/sections/WordGallery";
 import Button from "@/components/elements/Button";
 
