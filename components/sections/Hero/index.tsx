3a895268c8629e1e0c8901c4cbf7ccb77f95c6fd
davidhickey <davidhickey423@gmail.com>
2025-04-04 12:36:03 -0400

    updates lyrics game function naming

--- components/sections/Hero/index.tsx
+++ components/sections/Hero/index.tsx
@@ -1,9 +1,4 @@
-const HeroSection = ({
-  children
-}:{
-  children?: React.ReactNode
-}) => {
-
+const HeroSection = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div className="hero-section-container text-lPrimaryGreen dark:text-dPrimaryGray bg-lSecCream dark:bg-dSecDarkBlue transition-all duration-300 ease-in-out">
       <div className="relative isolate px-6 lg:pt-14 lg:px-8">
@@ -14,7 +9,7 @@ const HeroSection = ({
           <div
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-lSecCream to-lSecCream  dark:from-dSecBlack dark:to-dSecMaize opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
@@ -28,14 +23,14 @@ const HeroSection = ({
           <div
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-lSecCream to-lSecCream dark:from-dSecBlack dark:to-dSecMaize opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HeroSection;
