f03fe2dbb9f9db3527f4f18f1b6380c54937c82d
davidhickey <davidhickey423@gmail.com>
2025-04-17 12:24:09 -0400

    fixes build issues

--- app/pages/[slug]/page.tsx
+++ app/pages/[slug]/page.tsx
@@ -1,13 +1,17 @@
-import { getPagesSlugQuery, getPageBySlugQuery, sanityFetch   } from "@/lib/sanity/sanity.query";
+import { getPagesSlugQuery, getPageBySlugQuery, sanityFetch } from "@/lib/sanity/sanity.query";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
+type Props = {
+  params: Promise<{ slug: string }>;
+};
 export async function generateStaticParams(): Promise<{ slug: string }[]> {
   const data = await sanityFetch({query: getPagesSlugQuery});
   return data;
 }
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-  const { slug } = await params;
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const { slug } = await props.params;
   const page = await sanityFetch({query: getPageBySlugQuery, params: { slug }});
   return {
     title: `${page.title} | DH`,
@@ -15,8 +19,20 @@ export async function generateMetadata({ params }: { params: { slug: string } })
   } satisfies Metadata;
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
-  const page = await sanityFetch({query: getPageBySlugQuery, params: { slug }});
+export default async function Page(props: Props) {
+  const getPage = async () => {
+    try {
+      const { slug } = await props.params;
+      const page = await sanityFetch({query: getPageBySlugQuery, params: { slug }});
+      return page;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+  const page = await getPage();
+  if (!page) {
+    return notFound();
+  }
   return <div>{page.title}</div>;
 }
