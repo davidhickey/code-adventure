13c5fec472431d8d5b60d8a7a7cb991a1f215e52
davidhickey <davidhickey423@gmail.com>
2025-04-23 08:52:10 -0400

    fixes home page styling more

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -4,10 +4,10 @@ import SpiralGalaxy from "../sections/SpiralGalaxy";
 const HomePage = async ({children}:{children?: React.ReactNode}) => {
   return (
     <div>
-      <div className="h-screen w-full overflow-hidden absolute top-0 left-0 hidden dark:block bg-lSecCream dark:bg-dSecDarkBlue transition-all duration-300 ease-in-out">
+      <div className="h-[100dvh] w-full overflow-hidden absolute top-0 left-0 hidden dark:block bg-lSecCream dark:bg-dSecDarkBlue transition-all duration-300 ease-in-out">
         <SpiralGalaxy />
       </div>
-      <div className="absolute h-screen top-0 left-0 right-0 py-40 flex items-center dark:items-start justify-self-center sm:py-60">
+      <div className="absolute h-full top-0 left-0 right-0 py-40 flex items-center dark:items-start justify-self-center sm:py-60">
         <h1 className="text-center w-full text-5xl sm:text-8xl text-lPrimaryGreen dark:text-dPrimaryGray">{`Welcome :)`}</h1>
       </div>
       {children && <div>{children}</div>}
